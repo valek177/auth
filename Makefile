3e3ad4e4be6a6e9830bea301b213c6f111e58e89
valek177 <valek177@gmail.com>
2024-10-10 16:58:12 +0300

    Домашнее задание 1 неделя (auth) (#1)
    Week 1: add auth service, fix Makefile, go.mod, go.sum, add user.proto

--- Makefile
+++ Makefile
@@ -1,7 +1,27 @@
 LOCAL_BIN:=$(CURDIR)/bin
 
 install-golangci-lint:
-	GOBIN=$(LOCAL_BIN) go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.61.0
+	GOBIN=$(LOCAL_BIN) go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.60.3
 
 lint:
 	$(LOCAL_BIN)/golangci-lint run ./... --config .golangci.pipeline.yaml
+
+install-deps:
+	GOBIN=$(LOCAL_BIN) go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.34.2
+	GOBIN=$(LOCAL_BIN) go install -mod=mod google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+
+get-deps:
+	go get -u google.golang.org/protobuf/cmd/protoc-gen-go
+	go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
+generate:
+	make generate-user-api
+
+generate-user-api:
+	mkdir -p grpc/pkg/user_v1
+	protoc --proto_path grpc/api/user_v1 \
+	--go_out=grpc/pkg/user_v1 --go_opt=paths=source_relative \
+	--plugin=protoc-gen-go=bin/protoc-gen-go \
+	--go-grpc_out=grpc/pkg/user_v1 --go-grpc_opt=paths=source_relative \
+	--plugin=protoc-gen-go-grpc=bin/protoc-gen-go-grpc \
+	grpc/api/user_v1/user.proto
--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,92 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
+	"github.com/valek177/auth/grpc/pkg/user_v1"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	user_v1.UnimplementedUserV1Server
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	user_v1.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+// CreateUser creates new user with specified parameters
+func (s *server) CreateUser(_ context.Context, req *user_v1.CreateUserRequest) (
+	*user_v1.CreateUserResponse, error,
+) {
+	log.Printf("Create new user with name %s and email %s", req.GetName(), req.GetEmail())
+
+	return &user_v1.CreateUserResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+// GetUser returns info about user
+func (s *server) GetUser(_ context.Context, req *user_v1.GetUserRequest) (
+	*user_v1.GetUserResponse, error,
+) {
+	id := req.GetId()
+	log.Printf("Get user info by id: %d", id)
+
+	return &user_v1.GetUserResponse{
+		User: &user_v1.User{
+			Id: id,
+			UserInfo: &user_v1.UserInfo{
+				Name:  wrapperspb.String(gofakeit.Name()),
+				Email: wrapperspb.String(gofakeit.Email()),
+				Role:  user_v1.Role_USER,
+			},
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+// UpdateUser updates user info by id
+func (s *server) UpdateUser(_ context.Context, req *user_v1.UpdateUserRequest) (
+	*emptypb.Empty, error,
+) {
+	id := req.GetId()
+	log.Printf("Update user info by id: %d", id)
+
+	return &emptypb.Empty{}, nil
+}
+
+// DeleteUser removes user
+func (s *server) DeleteUser(_ context.Context, req *user_v1.DeleteUserRequest) (
+	*emptypb.Empty, error,
+) {
+	id := req.GetId()
+	log.Printf("Delete user by id: %d", id)
+
+	return &emptypb.Empty{}, nil
 }
